2f9153b6de21b3d52497d3e891c0252700c73103
chaitanyashirodkar010 <chaitanyashirodkar010@gmail.com>
2024-06-01 17:45:24 +0530

    if ($(".content:hidden").length == 0) {             $(".loadmorebutton").addClass("d-none");         }

--- footer.php
+++ footer.php
@@ -273,10 +273,15 @@ $('#playerSearch').on('keyup', function() {
 <script>
 $(document).ready(function() {
     $(".content").slice(0, 9).show();
+    $(".content2").slice(0, 12).show();
     $(".loadmorebutton").on("click", function(e) {
         e.preventDefault();
         $(".content:hidden").slice(0, 4).slideDown();
+        $(".content2:hidden").slice(0, 4).slideDown();
         if ($(".content:hidden").length == 0) {
+            $(".loadmorebutton").addClass("d-none");
+        }
+        if ($(".content2:hidden").length == 0) {
             $(".loadmorebutton").addClass("d-none");
         }
     });
