12128e9bb4976115e45821bca8c1b2db80ab9159
chaitanyashirodkar010 <chaitanyashirodkar010@gmail.com>
2024-06-01 17:45:52 +0530

    content2

--- page-player-auction.php
+++ page-player-auction.php
@@ -9,7 +9,7 @@
 
     <?php include 'components/page-title-header.php';?>
 <style>
-    .content{
+    .content2{
         display: none;
     }
 </style>
@@ -161,7 +161,7 @@
             <?php if (have_rows("gallery")):
                         while (have_rows("gallery")):
                              the_row(); ?>
-                <div class="col-md-3 mt-4 content"><a
+                <div class="col-md-3 mt-4 content2"><a
                         href="<?php the_sub_field("gallery_image"); ?>"
                         class="glightbox w-100" data-glightbox="type: image">
                         <img src="<?php the_sub_field("gallery_image"); ?>"
