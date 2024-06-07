68f09937e8aec6a27601a202377034f7284d96de
Baban Ghadi <ghadibaban08@gmail.com>
2024-06-07 12:56:49 +0530

    adding changes in time in fixture

--- page-new-fixtures.php
+++ page-new-fixtures.php
@@ -101,7 +101,7 @@
                         <div class="fixtures-schedule">
                             <div class="fixtures-schedule-date">
                                 <!-- THU, 15TH JUNE <b>19:30</b> -->
-                                <?php echo date("D, jS F", strtotime(convertGmtToClientTimezone($values['match_date_time'],'Asia/Kolkata')));?>
+                                <?php echo date("D, jS F H:i", strtotime(convertGmtToClientTimezone($values['match_date_time'],'Asia/Kolkata')));?>
                             </div>
                             <!-- TODO bind the location-->
                             <?php echo $values['ground_name']; ?>
